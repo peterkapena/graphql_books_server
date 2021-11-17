bf874898f5eda76b6b7b80eee0b987eb65f506c4
Peter Lumumba <Peter@devman3.com>
2021-11-17 10:35:20 +0200

    schema setup

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const app = require('express')()
 const { graphqlHTTP } = require('express-graphql')
+const schema = require('./schema/schema')
 
 app.use('/graphql', graphqlHTTP({
-    
+    schema
 }))
 
 app.listen(4000, () => console.log('I am running on port 4000'))
--- schema/schema.js
+++ schema/schema.js
@@ -1,1 +1,40 @@
-const { graphqlHTTP } = require('express-graphql')
+const { GraphQLObjectType, GraphQLSchema, GraphQLString } = require('graphql')
+
+const books = [
+    { name: 'Name of wind', genre: 'Fantasy', id: 1 },
+    { name: 'The final empire', genre: 'Fantasy', id: 3 },
+    { name: 'The long earth', genre: 'Action', id: 3 },
+]
+
+const BookType = new GraphQLObjectType({
+    name: 'Book',
+    fields: () => ({
+        id: { type: GraphQLString },
+        name: { type: GraphQLString },
+        genre: { type: GraphQLString },
+        // author: { type: AuthorType }
+    })
+})
+
+const AuthorType = new GraphQLObjectType({
+    name: 'Book',
+    fields: () => ({
+        id: { type: GraphQLString },
+    })
+})
+
+module.exports = new GraphQLSchema({
+    query: new GraphQLObjectType({
+        name: 'RootQuery',
+        fields: {
+            book: {
+                type: BookType,
+                args: { id: { type: GraphQLString } },
+                resolve(_, args) {
+                    books.find(b => b.id === args.id)
+                }
+            },
+        }
+    })
+})
+
