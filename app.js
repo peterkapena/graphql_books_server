e46b219c9bbb7aef3274f149c7567634f3f829ce
Peter Lumumba <Peter@devman3.com>
2021-11-17 09:39:26 +0200

    gitignore

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const app = require('express')()
 const { graphqlHTTP } = require('express-graphql')
 
 app.use('/graphql', graphqlHTTP({
-
+    
 }))
 
 app.listen(4000, () => console.log('I am running on port 4000'))
